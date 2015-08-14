2580221f05e1eb765ab74431ed15ba584d7bc299
go3001 <zhang1996@gmail.com>
2015-08-14 17:13:37 +1200

    Update prototypes20.c

--- prototypes20.c
+++ prototypes20.c
@@ -2,6 +2,16 @@
 prototypesxx.h
 */
 
-void
-UART_Init(unsigned int xyz);
-void UART_Transmit(uint8_t data );
+// insert comment
+void UART_Init(unsigned int xyz);
+
+
+
+// CreateDataPackage
+void CreateDataPackage( ... )
+
+// insert comment
+void UART_Transmit(uint8_t data);
+
+// insert comment
+void UART_Transmit();
